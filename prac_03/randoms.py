8812782f92044982ca300e32b219c51d52972cdc
Cody Toovey <ctoov9@eq.edu.au>
2025-09-30 14:59:51 +1000

    Add completed prac 03 randoms program and answers

---
+++ prac_03/randoms.py
@@ -0,0 +1,17 @@
+""""
+1. 20
+smallest - 5
+largest - 20
+
+2. 7
+smallest - 3
+largest - 9
+
+3. 4.11622335513286
+smallest - 5
+largest - 20
+"""
+
+from random import randint
+
+random_number = randint(1,100)
