adbe35650273b9e0983c6fab40d0a3a9a559f325
Cody Toovey <ctoov9@eq.edu.au>
2025-10-19 22:00:02 +1000

    Add load_records function to prac 5 wimbledon.py

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -4,3 +4,23 @@ Estimate: 30 minutes
 Actual:    minutes
 """
 
+FILENAME = "wimbledon.csv"
+
+
+def main():
+    """Read data file and print details."""
+    records = load_records(FILENAME)
+
+
+def load_records(filename):
+    """Load records from file."""
+    records = []
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()  # Remove CSV header row
+        for line in in_file:
+            parts = line.strip().split(",")
+            records.append(parts)
+    return records
+
+
+main()
