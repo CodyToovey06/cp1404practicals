5bfaa89042b0a158e937e11ac47037c0ad84ddb5
Cody Toovey <ctoov9@eq.edu.au>
2025-10-19 20:31:56 +1000

    Add looping in prac 5 state_names.py

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,10 +4,12 @@ State names in a dictionary
 File needs reformatting
 """
 
-
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
-print(CODE_TO_NAME)
+
+longest_code = max(len(code) for code in CODE_TO_NAME)
+for code in CODE_TO_NAME:
+    print(f"{code:{longest_code}} is {CODE_TO_NAME[code]}")
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
@@ -16,6 +18,3 @@ while state_code != "":
     else:
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
-
-
-
