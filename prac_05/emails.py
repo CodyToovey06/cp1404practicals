4edd077570417e4730b8b67eec27b28a883e03d0
Cody Toovey <ctoov9@eq.edu.au>
2025-10-19 21:30:24 +1000

    Complete main and determine_name_from_email functions for prac 5 emails.py

---
+++ prac_05/emails.py
@@ -0,0 +1,32 @@
+"""
+Emails
+Estimate: 10 minutes
+Actual:   11 minutes
+"""
+
+
+def main():
+    """Creates dictionary of names from emails."""
+    email_to_name = {}
+
+    email = input("Email: ")
+    while email != "":
+        name = determine_name_from_email(email)
+        confirmation = input(f"Is this your name {name}? (Y/n) ")
+        if confirmation.upper() != "Y" and confirmation != "":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
+
+    determine_name_from_email(email)
+
+
+def determine_name_from_email(email):
+    """Extract name from email"""
+    prefix = email.split('@')[0]
+    parts = prefix.split('.')
+    name = " ".join(parts).title()
+    return name
+
+
+main()
