03b7d299c8c54d77714085abead3f8bed863a493
Cody Toovey <cody.toovey@my.jcu.edu.au>
2025-11-02 18:43:52 +1000

    Refactor prac 6 guitar_test.py to be in run_tests function

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,15 +1,17 @@
-# Gibson L-5 CES get_age() - Expected 100. Got 100
-# Another Guitar get_age() - Expected 9. Got 9
-# Gibson L-5 CES is_vintage() - Expected True. Got True
-# Another Guitar is_vintage() - Expected False. Got False
 from guitar import Guitar
 
 
-guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
-other = Guitar("Another Guitar", 2012, 1512.9)
+def run_tests():
+    """Tests Guitar class."""
 
-print(f"{guitar.name} get_age() - Expected {103}. Got {guitar.get_age()}")
-print(f"{other.name} get_age() - Expected {13}. Got {other.get_age()}")
-print()
-print(f"{guitar.name} is_vintage() - Expected {True}. Got {guitar.is_vintage()}")
-print(f"{other.name} is_vintage() - Expected {False}. Got {other.is_vintage()}")
+    guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    other = Guitar("Another Guitar", 2012, 1512.9)
+
+    print(f"{guitar.name} get_age() - Expected {103}. Got {guitar.get_age()}")
+    print(f"{other.name} get_age() - Expected {13}. Got {other.get_age()}")
+    print()
+    print(f"{guitar.name} is_vintage() - Expected {True}. Got {guitar.is_vintage()}")
+    print(f"{other.name} is_vintage() - Expected {False}. Got {other.is_vintage()}")
+
+
+run_tests()
