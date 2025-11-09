03b9b0114a62079903f905df68b2e644da11222c
Cody Toovey <cody.toovey@my.jcu.edu.au>
2025-11-09 23:31:09 +1000

    Add save_projects function to prac 07 project_management.py

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -15,7 +15,24 @@ def main():
     print("Welcome to Pythonic Project Management")
     print(f"Loaded {len(projects)} projects from {FILENAME}")
     print(MENU)
-    display_projects(projects)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+            load_projects(FILENAME)
+        elif choice == "S":
+            save_projects(projects)
+        elif choice == "D":
+            display_projects(projects)
+        elif choice == "F":
+            pass
+        elif choice == "A":
+            pass
+        elif choice == "U":
+            pass
+        else:
+            print("Invalid menu choice")
+        print(MENU)
+        choice = input(">>> ").upper()
 
 
 def load_projects(filename):
@@ -26,7 +43,6 @@ def load_projects(filename):
         in_file.readline()
         for line in in_file:
             parts = line.strip().split('\t')
-            print(len(parts))
             projects.append(Project(parts[0], parts[1], int(parts[2]), float(parts[3]), int(parts[4])))
     return projects
 
@@ -41,6 +57,17 @@ def display_projects(projects):
     complete_projects = [project for project in projects if Project.is_complete(project)]
     for project in complete_projects:
         print(f" {project}")
+
+
+def save_projects(projects):
+    """Save projects list to chosen file."""
+    save_file = input("What file would you like to save to? ")
+    with open(save_file, "w") as out_file:
+        print("Name\tStart Date\tPriority\tCost Estimate\tCompletion Percentage", file=out_file)
+        for project in projects:
+            print(f"{project.name}\t{project.start_date.strftime('%d/%m/%Y')}\t{project.priority}"
+                  f"\t{project.cost_estimation}\t{project.completion_percentage}", file=out_file)
+        print(f"{len(projects)} projects saved to {save_file}")
 
 
 main()
