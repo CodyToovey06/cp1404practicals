062239d97c9cf087a7a0b6780f5f58adb60c3648
Cody Toovey <cody.toovey@my.jcu.edu.au>
2025-11-09 17:57:02 +1000

    Refactor prac 07 guitar_reader.py to have load_guitars function

--- prac_07/guitar_reader.py
+++ prac_07/guitar_reader.py
@@ -4,19 +4,23 @@ FILENAME = 'guitars.csv'
 
 
 def main():
-    """Read guitars from file and store them in a list of Guitar objects."""
+    """Program that reads guitars from file and store them in a list of Guitar objects."""
+    guitars = load_guitars(FILENAME)
+
+    guitars = add_guitars(guitars)
+    for guitar in guitars:
+        print(guitar)
+
+
+def load_guitars(filename):
     guitars = []
-    in_file = open(FILENAME, 'r')
+    in_file = open(filename, 'r')
     for line in in_file:
         parts = line.strip().split(',')
         guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
         guitars.append(guitar)
     in_file.close()
-
-    guitars = add_guitars(guitars)
-
-    for guitar in guitars:
-        print(guitar)
+    return guitars
 
 
 def add_guitars(guitars):
@@ -29,10 +33,13 @@ def add_guitars(guitars):
         guitars.append(guitar_to_add)
         print(guitar_to_add, "added.")
         name = input("Name: ")
-        with open(FILENAME, 'w') as outfile:
-            print(guitars, file=outfile)
+        with open(FILENAME, 'a') as outfile:
+            print(f"{name}, {cost}, {year}", file=outfile)
     guitars.sort()
     return guitars
+
+
+
 
 
 main()
