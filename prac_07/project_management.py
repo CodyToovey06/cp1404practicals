f1c017f72a837d1d5de0281b2c66fc263a1d8845
Cody Toovey <cody.toovey@my.jcu.edu.au>
2025-11-09 23:26:49 +1000

    Add display_projects function to prac 07 project_management.py

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -5,7 +5,7 @@ Actual time:
 
 from project import Project
 
-FILENAME = "what.txt"
+FILENAME = "projects.txt"
 MENU = "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (A)dd new project\n- (U)pdate project\n- (Q)uit"
 
 
@@ -21,16 +21,26 @@ def main():
 def load_projects(filename):
     """Load projects from CSV file."""
     projects = []
+
     with open(filename, "r") as in_file:
         in_file.readline()
         for line in in_file:
-            parts = line.strip().split('    ')
+            parts = line.strip().split('\t')
+            print(len(parts))
             projects.append(Project(parts[0], parts[1], int(parts[2]), float(parts[3]), int(parts[4])))
     return projects
 
 
 def display_projects(projects):
     """Display projects to user."""
+    print("Incomplete projects:")
+    incomplete_projects = [project for project in projects if not Project.is_complete(project)]
+    for project in incomplete_projects:
+        print(f" {project}")
+    print("Complete projects:")
+    complete_projects = [project for project in projects if Project.is_complete(project)]
+    for project in complete_projects:
+        print(f" {project}")
 
 
 main()
