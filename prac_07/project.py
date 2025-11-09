c47f136c5aa67190a00751a06f9af46e09da3972
Cody Toovey <cody.toovey@my.jcu.edu.au>
2025-11-09 23:21:41 +1000

    Add repr to prac 07 project.py

--- prac_07/project.py
+++ prac_07/project.py
@@ -12,8 +12,12 @@ class Project:
     def __str__(self):
         """Return a string representation of a Project."""
 
-        return (f"{self.name}, start: {self.start_date.strftime('%d/%m/%y')}, priority {self.priority}, "
+        return (f"{self.name}, start: {self.start_date}, priority {self.priority}, "
                 f"estimate: ${self.cost_estimation:.2f}, completion: {self.completion_percentage}%")
+
+    def __repr__(self):
+        """Return formatted string representation of Project object."""
+        return str(self)
 
     def is_complete(self):
         """Return True or False based on completion percentage."""
