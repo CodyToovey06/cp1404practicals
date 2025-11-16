3805a233d55fef0aed7b9b74fd802ffc98c90bb2
Cody Toovey <ctoov9@eq.edu.au>
2025-11-16 22:22:47 +1000

    Complete prac 08 Miles to Kilometres Converter stage 2

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -19,17 +19,25 @@ class ConvertMilesToKmApp(App):
 
     def handle_calculation(self):
         """Handle Kilometers conversion."""
-        miles = int(self.root.ids.input_miles.text)
+        miles = self.get_valid_number()
         self.handle_update(miles)
 
     def handle_increment(self, increment):
         """Update text input based on up and down button increments."""
-        miles = int(self.root.ids.input_miles.text) + increment
+        miles = self.get_valid_number() + increment
         self.root.ids.input_miles.text = str(miles)
 
     def handle_update(self, miles):
         """Update display with calculated conversions."""
         self.output_message = str(miles * MILES_TO_KM_CONVERSION)
+
+    def get_valid_number(self):
+        """Get valid float from text input."""
+        try:
+            value = float(self.root.ids.input_miles.text)
+            return value
+        except ValueError:
+            return 0.0
 
 
 ConvertMilesToKmApp().run()
